4243da2f3ad1752784948e69a9226a14ce11878f
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-05-08 21:06:10 +0200

    Switched to very smart textarea

--- src/Editor.svelte
+++ src/Editor.svelte
@@ -1,40 +1,42 @@
 <script lang="ts">
-import { globalShortcut } from "@tauri-apps/api";
+    import { globalShortcut } from "@tauri-apps/api";
 
     import { cleanHtml } from "../src/cleaner";
+import TextAreaAutoResize from "./TextAreaAutoResize.svelte";
 
-    let htmlContent: string;
-    export let textContent: string = "Enter your content here";
+    let htmlContent: string = "";
+    export const textContent: string = "Enter your content here";
 
-    $: htmlContent = textContent.split("\n").map((line) => `<div>${line}</div>`).join("\n")
+    $: htmlContent = textContent;
 
     export let cleanedHtml: string[];
 
-    $: cleanedHtml = cleanHtml(htmlContent);
+    // $: cleanedHtml = cleanHtml(htmlContent);
+    $: cleanedHtml = htmlContent.split("\n");
 
-    let previousContents: string[] = []
+    let previousContents: string[] = [];
 
     setInterval(() => {
+        previousContents.push(htmlContent);
 
-        previousContents.push(htmlContent)
-
-
-        if (previousContents.length > 100) previousContents.shift()
-    }, 2000)
+        if (previousContents.length > 100) previousContents.shift();
+    }, 2000);
 
     globalShortcut.isRegistered("Cmd+Z").then((registered) => {
         if (!registered) {
             globalShortcut.register("Cmd+Z", () => {
-                const newContent = previousContents.pop()
-                previousContents.push(newContent)
-                htmlContent = newContent
-            })
+                const newContent = previousContents.pop();
+                previousContents.push(newContent);
+                htmlContent = newContent;
+            });
         }
-    })
+    });
+
+
 </script>
 
 <div class="main">
-    <div contenteditable="true" bind:innerHTML={ htmlContent } spellcheck="false"></div>
+    <TextAreaAutoResize bind:value={htmlContent} maxRows=100000000000 />
 </div>
 
 <style>
@@ -44,5 +46,27 @@ import { globalShortcut } from "@tauri-apps/api";
         border-width: 1px;
         border-style: solid;
         border-radius: 10px;
+    }
+
+    .writearea {
+        width: 100%;
+        height: 100%;
+        margin-bottom: 0%;
+        background-color: black;
+        border: none;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .writearea {
+            background-color: black;
+            color: lightgrey;
+        }
+    }
+
+    @media (prefers-color-scheme: light) {
+        .writearea {
+            background-color: white;
+            color: black;
+        }
     }
 </style>
